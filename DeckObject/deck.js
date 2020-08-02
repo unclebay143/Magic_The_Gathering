2a5152cba23293c8bfe4593024ac898831fa8a78
David <davidffraser@Davids-MBP.lan>
2020-08-01 18:43:34 -0600

    put individual Deck prototypes into single prototype

--- DeckObject/deck.js
+++ DeckObject/deck.js
@@ -2,22 +2,22 @@
 function Deck(list){
   this.list = list
 }
-Deck.prototype.count = function () {
-  return this.list.length
-};
-Deck.prototype.printList = function () {
-  for(let i in this.list){
-    console.log(this.list[i])
+Deck.prototype = {
+  count : function () { return this.list.length },
+  printList : function () {
+    for(let i in this.list){
+      console.log(this.list[i])
+    }
+  },
+  draw : function () {
+    if(this.list.length > 0){
+      let randomIndex = Math.ceil(Math.random() * (this.count()-1))
+      let drawnCard = this.list[randomIndex]
+      this.list.splice(randomIndex,1)
+      return drawnCard
+    } return 'deck empty'
   }
-};
-Deck.prototype.draw = function () {
-  if(this.list.length > 0){
-  let randomIndex = Math.ceil(Math.random() * (this.count()-1))
-  let drawnCard = this.list[randomIndex]
-  this.list.splice(randomIndex,1)
-  return drawnCard
-} return 'deck empty'
-};
+}
 
 
 //Tests:
