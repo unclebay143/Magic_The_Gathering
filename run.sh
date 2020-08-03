e6f3a26c1e6da53481d6223ad38c2430997eb919
David <davidffraser@Davids-MBP.lan>
2020-08-03 10:50:35 -0600

    Moved compiled.js to root directory

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Because include/export is a pain lol
-cat ./Classes/*.js > /Users/davidffraser/Magic_The_Gathering/GameLogic/compiled.js
-cat ./GameLogic/gameLogic.js >> ./GameLogic/compiled.js
+cat ./Classes/*.js > /Users/davidffraser/Magic_The_Gathering/compiled.js
+cat ./GameLogic/gameLogic.js >> ./compiled.js
 
 open index.html
